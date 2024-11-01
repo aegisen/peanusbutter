4537324352c06e06c517401b60397ad7fd3764f7
Cynthia Lam <calam@wm.edu>
2024-10-31 22:45:41 -0400

    login added, can see playlists

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect } from "react";
+import GetPlaylists from "./components/GetPlaylists";
+
+const CLIENT_ID = "d511528d911b44e9a81863869ee60809";
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL = "http://localhost:3000/";
+const SPACE_DELIM = "%20";
+const SCOPE = [
+                "playlist-read-private",
+                "playlist-read-collaborative",
+                "playlist-modify-public",
+                "playlist-modify-private"
+              ];
+const SCOPES_URL_PARAM = SCOPE.join(SPACE_DELIM);
+
+
+const getParamsFromAuth = (hash) =>
+{
+  const stringAferHash = hash.substring(1);
+  const paramsInUrl = stringAferHash.split("&");
+  const paramsSplitUp = paramsInUrl.reduce((acc, current) => {
+    const [key, value] = current.split("=");
+    acc[key] = value;
+    return acc;
+  }, {});
+  return paramsSplitUp;
+}
+
+const App = () => {
+  useEffect( () => {
+    if(window.location.hash) {
+      const {access_token, expires_in, token_type} = getParamsFromAuth(window.location.hash);
+      window.location.hash = "";
+
+      console.log(access_token);
+
+      localStorage.clear(); //store our stuff bc idk im silly
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("tokenType", token_type);
+      localStorage.setItem("expiresIn", expires_in);
+    }
+  });
+
+
+  const handleLogin = () => {
+    window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+    };
+  return (
+      <div className="App">
+        <h1>hi</h1> 
+          <button onClick = {handleLogin}>Login</button>
+          <GetPlaylists />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/auth.js
@@ -0,0 +1,13 @@
+const clientID = 'd511528d911b44e9a81863869ee60809';
+const redirectURL = 'http://localhost:3000/';
+const scope = ["user-read-email", "playlist-modify-pubilc", "playlist-modify-private", "playlist-read-private", "playlist-read-collaborative"];
+
+
+var Spotify = require('spotify-web-api-js');
+
+
+
+
+export const authEndpoint = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURL}&scope=${perms.join('%20')}&response_type=token&show_dialog=false`;
+export const accessToken = window.location.hash.split('&')[0].split('=')[1];
+export const spotifyApi = new Spotify().setAccessToken(clientID);
